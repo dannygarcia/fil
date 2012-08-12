964549495c0526be5bd332e7ba07828f689991de
Danny Garcia <danny.d.g@gmail.com>
2012-08-11 23:05:24 -0700

    setting up crawler

--- src/main.js
+++ src/main.js
@@ -1,44 +1,98 @@
 require(["src/Canvas", "src/Input", "src/Pen", "src/Frame", "src/Transformer"], function (canvas, input, pen, frame, tr) {
-	var canvasNode = document.getElementById('canvas');
 
-	tr.transform(canvasNode, {
-		translateX: '10px'
-	});
-
-	canvas.init({
-		container : canvasNode
-	});
-
-	var ctx = canvas.context();
-
-	ctx.fillStyle = 'rgba(234, 46, 73, 1)';
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
-	ctx.fillStyle = ctx.strokeStyle = 'rgba(218,237,226,0.5)';
-
-	pen.context(ctx);
-
-	input.init();
-
-	frame.start();
-
-	var prev = {},
-		curr = {};
-
-	input.ontapstart = function (average) {
-		pen.circle(average, 10);
+	// Custom ajax function to avoid using jQuery (°-°)
+	var ajax = function (url, callback) {
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', url, true);
+		xhr.send();
+		xhr.onreadystatechange = function (e) {
+			if (e.target.status === 200 && e.target.readyState === 4) {
+				callback(JSON.parse(e.target.response));
+			}
+		};
 	};
 
-	input.ontapmove = function (average) {
-		curr = average;
+	var processor  = function () {
+
+		var self = this;
+
+		this.struct = [];
+
+		this.addBranch = function (array, item) {
+			ajax(item._links.self, function (data) {
+				self.processTree(array, data);
+			});
+		};
+
+		this.processTree = function (array, items) {
+
+			var item, i;
+
+			for (i = 0; i < items.length; i++) {
+				item = items[i];
+
+				if (item.type === 'dir') {
+					array[i] = {folder : []};
+					this.addBranch(array[i].folder, item);
+				} else {
+					array[i] = {item : item};
+				}
+
+			}
+
+		};
+
+		ajax('https://api.github.com/repos/dannyx0/danny-garcia/contents/', function (data) {
+			self.processTree(self.struct, data);
+		});
+
+		return this.struct;
+
 	};
 
-	input.ontapend = function (average) {
-		pen.circle(average, 10);
+	var s = new processor();
+	console.log(s);
+
+	var init = function () {
+
+		var canvasEl = document.getElementById('canvas');
+
+		// Start tracking mouse input on the whole doc.
+		input.init();
+
+		// Initialize the canvas.
+		canvas.init({
+			container : canvasEl
+		});
+
+		// Prep the context.
+		var ctx = canvas.context();
+		pen.context(ctx);
+
+		// Start requestAnimationFrame
+		frame.start();
+
+		var prev = {},
+			curr = {};
+
+		input.ontapstart = function (average) {
+			pen.circle(average, 10);
+		};
+
+		input.ontapmove = function (average) {
+			curr = average;
+		};
+
+		input.ontapend = function (average) {
+			pen.circle(average, 10);
+		};
+
+		frame.step = function (f) {
+			pen.circle(curr, 5);
+			pen.line(prev, curr);
+			prev = curr;
+		};
+
 	};
 
-	frame.step = function (f) {
-		pen.circle(curr, 5);
-		pen.line(prev, curr);
-		prev = curr;
-	};
 });
