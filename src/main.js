3c4212d39656ca6a607c1166a96524065f4b943b
Danny Garcia <danny.d.g@gmail.com>
2012-08-12 23:05:01 -0700

    first working version of github tree

--- src/main.js
+++ src/main.js
@@ -12,30 +12,55 @@ require(["src/Canvas", "src/Input", "src/Pen", "src/Frame", "src/Transformer"], 
 		};
 	};
 
-	var processor  = function () {
+	var buildGithubTree = function () {
 
 		var self = this;
 
 		this.struct = [];
 
+		this.drawNode = function (item, parent) {
+			if (parent) {
+				item.pos = {
+					x : parent.item.pos.x + Math.random() * 100,
+					y : parent.item.pos.y + Math.random() * 100
+				};
+			} else {
+				item.pos = {
+					x : Math.floor(Math.random() * (canvas.width - 0 + 1)) + 0,
+					y : Math.floor(Math.random() * (canvas.height - 0 + 1)) + 0
+				};
+			}
+			pen.circle(item.pos, item.type === 'dir' ? 10 : 5);
+
+		};
+
 		this.addBranch = function (array, item) {
 			ajax(item._links.self, function (data) {
-				self.processTree(array, data);
+				self.processTree(array.children, data, array);
 			});
 		};
 
-		this.processTree = function (array, items) {
+		this.processTree = function (array, items, parent) {
 
 			var item, i;
 
 			for (i = 0; i < items.length; i++) {
+
 				item = items[i];
 
+				array[i] = { item : item };
+
 				if (item.type === 'dir') {
-					array[i] = {folder : []};
-					this.addBranch(array[i].folder, item);
-				} else {
-					array[i] = {item : item};
+					array[i].children = [];
+					self.addBranch(array[i], array[i].item);
+				}
+
+				array[i].parent = parent ? parent : false;
+
+				this.drawNode(array[i].item, parent);
+
+				if (array[i].parent) {
+					pen.line(array[i].item.pos, array[i].parent.item.pos);
 				}
 
 			}
@@ -43,15 +68,29 @@ require(["src/Canvas", "src/Input", "src/Pen", "src/Frame", "src/Transformer"], 
 		};
 
 		ajax('https://api.github.com/repos/dannyx0/danny-garcia/contents/', function (data) {
-			self.processTree(self.struct, data);
+			self.processTree(self.struct, data, {
+				item : {
+					pos : {
+						x : canvas.width / 2,
+						y : canvas.height / 2
+					}
+				}
+			});
 		});
-
-		return this.struct;
 
 	};
 
-	var s = new processor();
-	console.log(s);
+	var node = function (pos, item) {
+
+		this.pos = pos;
+
+		this.draw = function (ctx) {
+
+			pen.circle(this.pos, item.type === 'folder' ? 10 : 5);
+
+		};
+
+	};
 
 	var init = function () {
 
@@ -67,32 +106,45 @@ require(["src/Canvas", "src/Input", "src/Pen", "src/Frame", "src/Transformer"], 
 
 		// Prep the context.
 		var ctx = canvas.context();
+		ctx.lineStroke = '#000';
 		pen.context(ctx);
-
-		// Start requestAnimationFrame
-		frame.start();
 
 		var prev = {},
 			curr = {};
 
-		input.ontapstart = function (average) {
-			pen.circle(average, 10);
-		};
+		// input.ontapstart = function (average) {
+		// 	pen.circle(average, 10);
+		// };
 
-		input.ontapmove = function (average) {
-			curr = average;
-		};
+		// input.ontapmove = function (average) {
+		// 	curr = average;
+		// };
 
-		input.ontapend = function (average) {
-			pen.circle(average, 10);
-		};
+		// input.ontapend = function (average) {
+		// 	pen.circle(average, 10);
+		// };
 
+		var nodes = [],
+			tree = new buildGithubTree();
+
+		// frame.start();
 		frame.step = function (f) {
-			pen.circle(curr, 5);
-			pen.line(prev, curr);
+
+			for (var i = 0; i < nodes.length; i++) {
+				nodes[i].prev = nodes[i - 1];
+				nodes[i].draw();
+			}
+
 			prev = curr;
+
+			if (f > 60) {
+				frame.stop();
+			}
+
 		};
 
 	};
+
+	init();
 
 });
